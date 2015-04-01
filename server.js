d286e6ab345a7800e2865bbe35ef9d377ba00b5e
Ross Cooperman <rcooperman@movableink.com>
2015-04-01 15:38:55 -0400

    Refresh list of card names when the server starts

--- server.js
+++ server.js
@@ -7,62 +7,76 @@ var fs = require('fs'),
 var slack = new Slack("xoxb-4235797741-tz6k3z8Y1qkiSmj4CJgRGaIt"),
     client = new Client(),
     index = new Index(),
-    userId = null,
-    names = JSON.parse(fs.readFileSync('names.json')).names;
+    userId = null;
 
-console.log('Indexing card names...');
+console.log('Fetching list of card names...');
 
-for (id in names) {
-  index.addDocument(id, {name: names[id]});
-}
+http.get('http://api.mtgapi.com/v2/names', function(res) {
+  var output = '';
 
-console.log('Ready for requests...');
+  res.on('data', function (chunk) {
+    output += chunk;
+  });
 
-client.on('connectFailed', function(error) {
-  console.log('error connecting', error);
-});
+  res.on('end', function() {
+    var names = JSON.parse(output).names;
 
-client.on('connect', function(connection) {
-  connection.on('message', function(message) {
-    data = JSON.parse(message.utf8Data);
-    if (data.user != userId && data.type == 'message') {
-      var results = index.query(data.text),
-          target = data.text.toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' '),
-          bestMatch = null;
+    console.log('Indexing card names...');
 
-      for (i in results) {
-        var test = results[i].doc.name.toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' ');
-
-        if (target.indexOf(test) >= 0 && (!bestMatch || results[i].doc.name.length > bestMatch.doc.name.length)) {
-          bestMatch = results[i];
-        }
-      }
-      if (bestMatch) {
-        http.get('http://api.mtgapi.com/v2/cards?name='+bestMatch.doc.name, function(res) {
-          var output = '';
-
-          res.on('data', function (chunk) {
-            output += chunk;
-          });
-
-          res.on('end', function() {
-            var card = JSON.parse(output).cards[0];
-
-            var attachments = JSON.stringify([{
-              title: bestMatch.doc.name,
-              title_link: 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid='+card.multiverseid,
-              image_url: card.images.gatherer
-            }]);
-
-            slack.api("chat.postMessage", {channel: data.channel, as_user: true, text: ' ', attachments: attachments}, function() {});
-          });
-        });
-      }
+    for (id in names) {
+      index.addDocument(id, {name: names[id]});
     }
+
+    console.log('Ready for requests...');
+
+    client.on('connectFailed', function(error) {
+      console.log('error connecting', error);
+    });
+
+    client.on('connect', function(connection) {
+      connection.on('message', function(message) {
+        data = JSON.parse(message.utf8Data);
+        if (data.user != userId && data.type == 'message') {
+          var results = index.query(data.text),
+              target = data.text.toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' '),
+              bestMatch = null;
+
+          for (i in results) {
+            var test = results[i].doc.name.toLowerCase().replace(/[^ \w]/g, ' ').replace(/ +/g, ' ');
+
+            if (target.indexOf(test) >= 0 && (!bestMatch || results[i].doc.name.length > bestMatch.doc.name.length)) {
+              bestMatch = results[i];
+            }
+          }
+          if (bestMatch) {
+            http.get('http://api.mtgapi.com/v2/cards?name='+bestMatch.doc.name, function(res) {
+              var output = '';
+
+              res.on('data', function (chunk) {
+                output += chunk;
+              });
+
+              res.on('end', function() {
+                var card = JSON.parse(output).cards[0];
+
+                var attachments = JSON.stringify([{
+                  title: bestMatch.doc.name,
+                  title_link: 'http://gatherer.wizards.com/Pages/Card/Details.aspx?multiverseid='+card.multiverseid,
+                  image_url: card.images.gatherer
+                }]);
+
+                slack.api("chat.postMessage", {channel: data.channel, as_user: true, text: ' ', attachments: attachments}, function() {});
+              });
+            });
+          }
+        }
+      });
+    });
+
+    slack.api("rtm.start", function(err, response) {
+      userId = response.self.id;
+      client.connect(response.url);
+    });
   });
 });
 
-slack.api("rtm.start", function(err, response) {
-  userId = response.self.id;
-  client.connect(response.url);
-});
